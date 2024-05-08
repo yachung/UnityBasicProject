13ff6c0888f63a4791e54bd4ec95042be156759e
yachung <dbstjd8267@gmail.com>
2024-05-08 11:32:53 +0900

    유니티 2일차 수업 첫시간 - 마테리얼

---
+++ Assets/Scripts/CustomCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCube : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Console 창에 문자를 출력해주는 메소드
+        Debug.Log("Update");
+    }
+}
