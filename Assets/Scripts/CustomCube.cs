464d3aa39038a72f3674c443b2643869622f939a
yachung <dbstjd8267@gmail.com>
2024-05-08 13:30:30 +0900

    간단한 이동로직 작성
    - CustomCube 스크립트에 이동 스크립트 작성

--- Assets/Scripts/CustomCube.cs
+++ Assets/Scripts/CustomCube.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class CustomCube : MonoBehaviour
 {
+    // 필드
+    public Transform refTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
+
+        //refTransform = GetComponent<Transform>();
+        refTransform = transform;
+
+
+        refTransform.Translate(1f, 0f, 0f);
     }
 
     // Update is called once per frame
@@ -16,5 +24,6 @@ public class CustomCube : MonoBehaviour
     {
         // Console 창에 문자를 출력해주는 메소드
         Debug.Log("Update");
+        refTransform.Translate(0.01f, 0f, 0f);
     }
 }
