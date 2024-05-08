b40528eee2acd5b69031867719260908ce5a402b
yachung <dbstjd8267@gmail.com>
2024-05-08 15:27:03 +0900

    이동과 회전을 모두 적용한 큐브 이동로직 작성
    - 앞뒤 이동 및 좌우 회전을 적용해서 이동기능을 추가.

--- Assets/Scripts/CustomCube.cs
+++ Assets/Scripts/CustomCube.cs
@@ -6,7 +6,8 @@ public class CustomCube : MonoBehaviour
 {
     // 필드
     [SerializeField] private Transform refTransform;
-    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float moveSpeed = 10f;         // 이동 속도
+    [SerializeField] private float rotationSpeed = 360f;    // 회전 속도(단위 초)
 
     // Start is called before the first frame update
     void Start()
@@ -16,18 +17,22 @@ public class CustomCube : MonoBehaviour
         //refTransform = GetComponent<Transform>();
         refTransform = transform;
 
-
         refTransform.Translate(1f, 0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Console 창에 문자를 출력해주는 메소드
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Debug.Log(horizontal);
 
-        refTransform.Translate(moveSpeed * horizontal * Time.deltaTime, moveSpeed * vertical * Time.deltaTime, 0f);
+        //refTransform.Translate(moveSpeed * horizontal * Time.deltaTime, moveSpeed * vertical * Time.deltaTime, 0f);
+
+        // 이동 Z축 -> 물체 기준 앞뒤 이동.
+        //refTransform.localPosition += new Vector3(0f, 0f, vertical * moveSpeed * Time.deltaTime);
+        refTransform.position += refTransform.forward * vertical * moveSpeed * Time.deltaTime;
+
+        refTransform.Rotate(new Vector3(0f, horizontal * rotationSpeed * Time.deltaTime, 0f));
     }
 }
